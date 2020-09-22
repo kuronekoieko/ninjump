268d934ecd0e470327d72c367cb7056c86b0d333
家子智成 <>
2020-09-22 22:44:56 +0900

    ジャンプで乗り移る処理

---
+++ Assets/0_MyAssets/Scripts/Game/JumperController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class JumperController : MonoBehaviour
+{
+    public UnityAction<Collider> SetOnCollisionEnter;
+    private void OnCollisionEnter(Collision other)
+    {
+        // SetOnCollisionEnter(other);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SetOnCollisionEnter(other);
+    }
+}
--- Assets/0_MyAssets/Scripts/Game/PlayerController.cs
+++ Assets/0_MyAssets/Scripts/Game/PlayerController.cs
@@ -12,27 +12,32 @@ public enum PlayerState
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] Transform jumperTf;
+    [SerializeField] JumperController jumperController;
     float speed = 0.2f;
     PlayerState playerState = PlayerState.Run;
     bool isRunRightWall;
     float wallsDistance;
+    Tween jumpTween;
+    float GetSign => isRunRightWall ? 1 : -1;
 
     void Start()
     {
         isRunRightWall = true;
-        wallsDistance = Mathf.Abs(transform.position.x) * 2;
+        wallsDistance = Mathf.Abs(jumperTf.position.x) * 2;
+        jumperController.SetOnCollisionEnter += OnHitWall;
     }
-
 
     private void FixedUpdate()
     {
+        transform.Translate(Vector3.up * speed);
+
         switch (playerState)
         {
             case PlayerState.Run:
-                transform.Translate(Vector3.right * speed);
                 if (Input.GetMouseButtonDown(0))
                 {
-
+                    Jump();
                 }
                 break;
             case PlayerState.Jump:
@@ -41,4 +46,30 @@ public class PlayerController : MonoBehaviour
                 break;
         }
     }
+
+    void Jump()
+    {
+        playerState = PlayerState.Jump;
+        Vector3[] path = new Vector3[]
+        {
+            new Vector3(wallsDistance * -GetSign/2f,2f,0),
+            new Vector3(wallsDistance * -GetSign,0,0),
+        };
+        jumpTween = jumperTf
+            .DOLocalPath(path, 0.5f, PathType.CatmullRom)
+            .SetRelative()
+            .SetEase(Ease.Linear);
+    }
+
+    void OnHitWall(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Wall")) return;
+        if (playerState != PlayerState.Jump) return;
+        playerState = PlayerState.Run;
+        isRunRightWall = !isRunRightWall;
+        //jumpTween.Kill();
+        jumperTf.RotateAround(jumperTf.position, Vector3.up, 180 * GetSign);
+        Debug.Log(0);
+    }
+
 }
