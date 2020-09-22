9b331a1201cfdd7eee44809f78e250a791b29de2
家子智成 <>
2020-09-22 20:40:46 +0900

    状態定義

--- Assets/0_MyAssets/Scripts/Game/CameraController.cs
+++ Assets/0_MyAssets/Scripts/Game/CameraController.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 using DG.Tweening;
 using Zenject;
 
-/// <summary>
-/// Unityで解像度に合わせて画面のサイズを自動調整する
-/// http://www.project-unknown.jp/entry/2017/01/05/212837
-/// </summary>
 public class CameraController : MonoBehaviour
 {
     [Inject] PlayerController playerController;
--- Assets/0_MyAssets/Scripts/Game/PlayerController.cs
+++ Assets/0_MyAssets/Scripts/Game/PlayerController.cs
@@ -1,19 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+
+public enum PlayerState
+{
+    Run,
+    Jump,
+}
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Animator animator;
-    float speed = 0.1f;
+    float speed = 0.2f;
+    PlayerState playerState = PlayerState.Run;
+    bool isRunRightWall;
+    float wallsDistance;
+
     void Start()
     {
-
+        isRunRightWall = true;
+        wallsDistance = Mathf.Abs(transform.position.x) * 2;
     }
 
 
     private void FixedUpdate()
     {
-        transform.Translate(Vector3.right * speed);
+        switch (playerState)
+        {
+            case PlayerState.Run:
+                transform.Translate(Vector3.right * speed);
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                }
+                break;
+            case PlayerState.Jump:
+                break;
+            default:
+                break;
+        }
     }
 }
